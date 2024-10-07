847dd316aab7a4b8da47df8eb70afbb6826038da
SergioEloy <sergioeloyespadas@gmail.com>
2024-10-06 18:58:18 -0600

    chore: landing page ready

---
+++ src/components/B4CFooter.tsx/B4CFooter.tsx
@@ -0,0 +1,51 @@
+import { Box, IconButton, Typography } from "@mui/material";
+import YouTubeIcon from "@mui/icons-material/YouTube";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import { colorPalette } from "@/style/partials/colorPalette";
+
+export const B4CFooter = () => {
+  return (
+    <Box
+      component={"footer"}
+      sx={{
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+        padding: "25px 40px",
+        backgroundColor: colorPalette.grey1,
+      }}
+    >
+      <Box>
+        <Typography
+          sx={{
+            color: colorPalette.white,
+          }}
+        >
+          © 2025 Bid4Care. Todos los derechos reservados
+        </Typography>
+      </Box>
+      <Box display={"flex"} gap={16}>
+        <IconButton
+          sx={{
+            backgroundColor: colorPalette.grey3,
+            color: colorPalette.white,
+          }}
+          href="https://www.youtube.com/"
+          target="_blank"
+        >
+          <YouTubeIcon />
+        </IconButton>
+        <IconButton
+          sx={{
+            backgroundColor: colorPalette.grey3,
+            color: colorPalette.white,
+          }}
+          href="https://www.linkedin.com/"
+          target="_blank"
+        >
+          <LinkedInIcon />
+        </IconButton>
+      </Box>
+    </Box>
+  );
+};
---
+++ src/constants/benefits.ts
@@ -0,0 +1,41 @@
+import { B4CBenefitsCardProps } from "@/components/B4CBenefitsCard/B4CBenefitsCard";
+
+export const benefits: B4CBenefitsCardProps[] = [
+  {
+    title: "Mantenimiento del hogar",
+    subtitle:
+      "Profesionales que se ocupan de reparaciones y limpieza en el hogar, incluyendo plomería, electricidad, pintura y jardinería.",
+    color: "red",
+  },
+  {
+    title: "Cuidado y asistencia",
+    subtitle:
+      "Servicios dedicados al cuidado de personas, como niños, adultos mayores y enfermos, brindando atención personalizada.",
+    color: "green",
+  },
+  {
+    title: "Servicios de salud",
+    subtitle:
+      "Profesionales en el área de la salud, como médicos, enfermeras y terapeutas, brindando atención médica y cuidados especializados.",
+    color: "yellow",
+  },
+  {
+    title: "Servicios técnicos",
+    subtitle:
+      "Profesionales en informática, redes y sistemas de seguridad, encargados de instalación y mantenimiento de equipos tecnológicos.",
+    color: "red",
+  },
+  {
+    title: "Ayudantes Generales",
+    subtitle:
+      "Trabajadores versátiles que realizan tareas de apoyo, como carga y limpieza.",
+    color: "green",
+  },
+
+  {
+    title: "Entretenimiento y eventos",
+    subtitle:
+      "Especialistas en la organización de eventos y actividades recreativas, como fiestas, conciertos y actividades deportivas.",
+    color: "yellow",
+  },
+];
