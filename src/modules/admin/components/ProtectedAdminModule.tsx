ab879224c7797146842c97870446fc7cb8725a50
Braulio95 <braulioestradaalfonso@hotmail.com>
2025-03-22 02:55:29 -0600

    hotfix

--- src/modules/admin/components/ProtectedAdminModule.tsx
+++
@@ -1,8 +0,0 @@
-import { useAdminAuth } from "@/context/auth/AuthAdminContext";
-
-import { Outlet, Navigate } from "react-router-dom";
-
-export const ProtectedModule = () => {
-  const { isAuthenticated } = useAdminAuth();
-  return isAuthenticated ? <Outlet /> : <Navigate to="/login" />;
-};
