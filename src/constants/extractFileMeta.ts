2db1b4ea48c4b38f2699833dc6fbbe6f88153408
Braulio95 <braulioestradaalfonso@hotmail.com>
2025-05-10 21:59:54 -0600

    pruebas de tipos

--- src/constants/extractFileMeta.ts
+++ src/constants/extractFileMeta.ts
@@ -4,7 +4,8 @@ export const extractFileMeta = (file: File): FileUploadMetadata => {
   const fullName = file.name;
   const lastDot = fullName.lastIndexOf(".");
   const name = lastDot > 0 ? fullName.substring(0, lastDot) : fullName;
-  let ext = lastDot > 0 ? fullName.substring(lastDot + 1) : "";
+  let ext =
+    lastDot > 0 ? fullName.substring(lastDot + 1).toLocaleLowerCase() : "";
 
   // 2️⃣ Normalizamos: si viene "jpg" lo convertimos a "jpeg"
   const normalizedType = ext === "jpg" ? "jpeg" : ext;
